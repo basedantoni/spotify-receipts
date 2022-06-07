6eb3d5f3a77c949651dc3fe6d3c7ed2fe1c6612d
Anthony Mercado <antho@Anthonys-Mini.attlocal.net>
2022-06-06 23:55:06 -0500

    :tada: App set up complete

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,15 +1,25 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
   ],
   env: {
-    "vue/setup-compiler-macros": true,
+    'vue/setup-compiler-macros': true,
+  },
+  rules: {
+    quotes: [2, 'single', { avoidEscape: true }],
+    unknownAtRules: 'ignore',
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+      },
+    ],
   },
 };
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="font-extrabold">Spotify Shit Home</h1>
   </main>
 </template>
